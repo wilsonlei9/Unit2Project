07460523d0a8581e9ce2fc93eb03a4a9b3bce8de
BT_4N2_22 <wlei6251@bths.edu>
2022-10-11 12:32:00 -0400

    Added EquationRunner class

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
 public class EquationRunner {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the first coordinate point: ");
+        String coordinate = s.nextLine();
+        System.out.print("Enter the second coordinate point: ");
+        String coordinate2 = s.nextLine();
+
+
+    }
 }
